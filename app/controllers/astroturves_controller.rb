9837b7dcb674f60f1e4a8347519b7d04c78639a6
Alperen Bozkurt <bozkurttalperen@gmail.com>
2016-08-26 12:47:08 +0300

    added index,show,new page for astroturves

--- app/controllers/astroturves_controller.rb
+++ app/controllers/astroturves_controller.rb
@@ -5,11 +5,28 @@ class AstroturvesController < ApplicationController
   end
 
   def new
-
+    @astroturf = Astroturf.new
   end
 
   def create
+    @astroturf = Astroturf.new(strong_params)
 
+    if @astroturf.save
+      flash[:success] =  'Halısaha başarılı bir şekilde eklendi..'
+      redirect_to astroturf_path(@astroturf)
+    else
+      render :new
+    end
   end
 
+  def show
+      @astroturf = Astroturf.find(params[:id])
+  end
+
+
+  private
+
+  def strong_params
+    params.require(:astroturf).permit(:name, :address, :city, :info, :phone, :map, :facebook, :twitter, :instagram)
+	end
 end
