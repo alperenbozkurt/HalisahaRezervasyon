0129a8d195fffa4132195b20507e6e6cec2a551e
Alperen Bozkurt <bozkurttalperen@gmail.com>
2016-08-26 16:00:30 +0300

    added update,delete page for astroturves

--- app/controllers/astroturves_controller.rb
+++ app/controllers/astroturves_controller.rb
@@ -1,5 +1,5 @@
 class AstroturvesController < ApplicationController
-
+  before_action :set_astroturf, only: [:show, :edit, :update, :destroy]
   def index
     @astroturves = Astroturf.all
   end
@@ -12,20 +12,41 @@ class AstroturvesController < ApplicationController
     @astroturf = Astroturf.new(strong_params)
 
     if @astroturf.save
-      flash[:success] =  'Halısaha başarılı bir şekilde eklendi..'
+      flash[:success] =  'Halısaha başarılı bir şekilde oluşturuldu..'
       redirect_to astroturf_path(@astroturf)
     else
+      flash[:error] =  'Halısaha Oluşturulamadı'
       render :new
     end
   end
 
   def show
-      @astroturf = Astroturf.find(params[:id])
+
   end
 
+  def edit
+    
+  end
 
+  def update
+    if @astroturf.update(strong_params)
+      flash[:success] = 'Halısaha başarılı bir şekilde güncellendi..'
+      redirect_to astroturf_path(@astroturf)
+    else
+      flash[:error] = 'Halısaha Güncellenemedi.'
+      render :edit
+    end
+  end
+
+  def destroy
+      @astroturf.destroy
+      flash[:success] = 'Halısaha başarılı bir şekilde silindi..'
+      redirect_to astroturves_path
+  end
   private
-
+  def set_astroturf
+    @astroturf = Astroturf.find(params[:id])
+  end
   def strong_params
     params.require(:astroturf).permit(:name, :address, :city, :info, :phone, :map, :facebook, :twitter, :instagram)
 	end
--- app/models/astroturf.rb
+++ app/models/astroturf.rb
@@ -1,2 +1,7 @@
 class Astroturf < ApplicationRecord
+  validates :name, presence: true
+  validates :address, presence: true
+  validates :city, presence: true
+  validates :info, presence: true
+  validates :phone, numericality: true
 end
